a2c4527f200556749f9144e344d20ba6f69b63ec
d.neustadt <d.neustadt@shopware.com>
2023-03-19 22:39:34 +0100

    feat: nice motivational greetings

--- src/Resources/app/administration/src/main.js
+++ src/Resources/app/administration/src/main.js
@@ -1,1 +1,7 @@
+import deDeSnippets from './snippet/de-DE.json';
+import enGBSnippets from './snippet/en-GB.json';
+
+Shopware.Locale.extend('de-DE', deDeSnippets);
+Shopware.Locale.extend('en-GB', enGBSnippets);
+
 import './styles.scss';
